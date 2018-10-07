93d9a42f143603af1f71be74fcf903c19ff321a9
Gabriel Stellini <gabrielstellini@gmail.com>
2018-10-07 12:55:49 +0200

    Add project config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
+import {AppRoutes} from './app.routes';
+import {MainModule} from './main/main.module';
+import {TooltipModule} from 'ngx-bootstrap';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ToastrModule} from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MainModule,
+    RouterModule.forRoot(AppRoutes),
+    TooltipModule.forRoot(),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
